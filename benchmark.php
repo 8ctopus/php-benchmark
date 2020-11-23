aa10b4e2657d7e099cf3e9b0dff298b8d3127846
8ctopus <hello@octopuslabs.io>
2020-11-23 16:57:57 +0500

    improved: tests now run against a time limit instead of running a fixed number of iterations fixed: error reporting when running from command line fixed: test error management

--- benchmark.php
+++ benchmark.php
@@ -11,30 +11,35 @@
  * @author 8ctopus <hello@octopuslabs.io>
  */
 
-// iterations
-$iterations = 25;
-
-// paddings
-$pad1 = 15;
-$pad2 = 27;
-$pad3 = 9;
-
-$line = str_pad('', $pad2, '-');
+// set error reporting
+error_reporting(E_ERROR /*| E_WARNING | E_PARSE*/);
 
 // check if running from cli
 if (php_sapi_name() != 'cli')
     echo('<pre>');
 
+// paddings
+$pad1     = 18;
+$pad2     =  9;
+$pad_line = $pad1 + $pad2 + 3;
+
+$line = str_pad('', $pad_line, '-');
+
+// iterations
+$iterations         = 10;
+$time_per_iteration = 1.000;
+
 echo('PHP benchmark' ."\n\n".
-    str_pad('php version', $pad1) .' : '. str_pad(PHP_VERSION, $pad3, ' ', STR_PAD_LEFT) ."\n".
-    str_pad('platform', $pad1) .' : '. str_pad(PHP_OS .' '. ((PHP_INT_SIZE == 8) ? 'x64' : 'x32'), $pad3, ' ', STR_PAD_LEFT) ."\n".
-    str_pad('memory limit', $pad1) .' : '. str_pad(ini_get('memory_limit'), $pad3, ' ', STR_PAD_LEFT) ."\n".
-    str_pad('max execution', $pad1) .' : '. str_pad(ini_get('max_execution_time'), $pad3, ' ', STR_PAD_LEFT) ."\n".
-    str_pad('iterations', $pad1) .' : '. str_pad($iterations, $pad3, ' ', STR_PAD_LEFT) ."\n".
+    str_pad('php version', $pad1) .' : '. str_pad(PHP_VERSION, $pad2, ' ', STR_PAD_LEFT) ."\n".
+    str_pad('platform', $pad1) .' : '. str_pad(PHP_OS .' '. ((PHP_INT_SIZE == 8) ? 'x64' : 'x32'), $pad2, ' ', STR_PAD_LEFT) ."\n".
+    str_pad('memory limit', $pad1) .' : '. str_pad(ini_get('memory_limit'), $pad2, ' ', STR_PAD_LEFT) ."\n".
+    str_pad('max execution', $pad1) .' : '. str_pad(ini_get('max_execution_time'), $pad2, ' ', STR_PAD_LEFT) ."\n".
+    str_pad('time per iteration', $pad1) .' : '. str_pad($time_per_iteration, $pad2, ' ', STR_PAD_LEFT) ."\n".
+    str_pad('iterations', $pad1) .' : '. str_pad($iterations, $pad2, ' ', STR_PAD_LEFT) ."\n".
     "$line\n"
 );
 
-$total     = 0;
+//$total = 0;
 
 // list functions
 $functions = get_defined_functions();
@@ -47,64 +52,80 @@ foreach ($functions['user'] as $user) {
 
         // run each test x times
         for ($i = 0; $i < $iterations; $i++) {
-            $timings[$i] = $user();
+            $timings[$i] = $user($time_per_iteration);
+
+            if ($timings[$i] === false) {
+                $error = true;
+                break;
+            }
         }
 
-        if ($timings[0] == -1) {
-            echo(str_pad($user, $pad1) .' : '. str_pad('FAILED', $pad3, ' ', STR_PAD_LEFT) ."\n");
+        // check for error
+        if (isset($error)) {
+            echo(str_pad($user, $pad1) .' : '. str_pad('FAILED', $pad2, ' ', STR_PAD_LEFT) ."\n");
             continue;
         }
 
-        $total += median($timings);
-        echo(str_pad($user, $pad1) .' : '. format_time(median($timings), $pad3, true) .' '. variability($timings) .' '. interval($timings) .' - '. show_all($timings) ."\n");
+//        $total += median($timings);
+        echo(str_pad($user, $pad1) .' : '. format_number(median($timings), $pad2, true) .' '. variability($timings) .' '. interval($timings) .' - '. show_all($timings) ."\n");
     }
 }
 
-echo(str_pad('-', $pad2, '-') ."\n".
+/* FIX ME total doesn't work for the time being
+echo(str_pad('-', $pad_line, '-') ."\n".
     str_pad('Total time ', $pad1) .' : '.
-    format_time($total, $pad3, true)
+    format_number($total, $pad2, true)
 );
+*/
 
 exit();
 
 
 /**
  * Test math functions
- * @param  int $iterations
- * @return int elapsed time
+ * @param  float $limit time limit in seconds
+ * @return int iterations done in allocated time
  */
-function test_math($iterations = 220000)
+function test_math($limit)
 {
-    $time_start  = microtime(true);
-    $functions   = ['abs', 'acos', 'asin', 'atan', 'decbin', 'exp', 'floor', 'exp', 'is_finite',
-        'is_nan', 'log', 'log10', 'log1p', 'sin', 'tan', 'pi', 'pow', 'sqrt'];
+    $time_start = microtime(true);
+    $time_limit = $time_start + $limit;
+    $iterations = 0;
+
+    $functions  = ['abs', 'acos', 'asin', 'atan', 'decbin', 'exp', 'floor', 'exp', 'is_finite',
+        'is_nan', 'log', 'log10', 'log1p', 'pi', 'pow', 'sin', 'sqrt', 'tan'];
 
     // remove functions that don't exist
     $functions = check_functions_exist($functions);
 
     // run tests
-    foreach ($functions as $function) {
-        // get function arguments count
-        $reflection = new ReflectionFunction($function);
-        $arguments  = $reflection->getNumberOfParameters();
+    while (microtime(true) < $time_limit) {
+        foreach ($functions as $function) {
+            // get function arguments count
+            $reflection = new ReflectionFunction($function);
+            $arguments  = $reflection->getNumberOfParameters();
 
-        for ($i = 0; $i < $iterations; $i++) {
-            call_user_func_array($function, $arguments == 1 ? [$i + 1] : [$i + 1, $i + 1]);
+            call_user_func_array($function, $arguments == 1 ? [$iterations + 1] : [$iterations + 1, $iterations + 1]);
         }
+
+        $iterations++;
     }
 
-    return microtime(true) - $time_start;
+    return $iterations;
 }
 
 
 /**
  * Test string functions
- * @param  int $iterations
- * @return int elapsed time
+ * @param  int $limit time limit in microseconds
+ * @return int iterations done in allocated time
  */
-function test_strings($iterations = 130000)
+function test_strings($limit)
 {
     $time_start = microtime(true);
+    $time_limit = $time_start + $limit;
+    $iterations = 0;
+
     $functions  = ['addslashes', 'chunk_split', 'ltrim', 'metaphone', 'ord', 'str_shuffle',
         'strip_tags', 'strlen', 'strtoupper', 'strtolower', 'strrev', 'soundex', 'trim'];
 
@@ -114,125 +135,142 @@ function test_strings($iterations = 130000)
     $string = 'the quick brown fox jumps over the lazy dog';
 
     // run tests
-    foreach ($functions as $function) {
-        for ($i = 0; $i < $iterations; $i++) {
+    while (microtime(true) < $time_limit) {
+        foreach ($functions as $function) {
             call_user_func_array($function, [$string]);
         }
+
+        $iterations++;
     }
 
-    return microtime(true) - $time_start;
+    return $iterations;
 }
 
 
 /**
  * Test loops
- * @param  int $iterations
- * @return int elapsed time
+ * @param  int $limit time limit in microseconds
+ * @return int iterations done in allocated time
  */
-function test_loops($iterations = 16500000)
+function test_loops($limit)
 {
     $time_start = microtime(true);
+    $time_limit = $time_start + $limit;
+    $iterations = 0;
 
-    $j = 0;
+    while (microtime(true) < $time_limit) {
+        $j = 0;
 
-    for ($i = 0; $i < $iterations; ++$i) {
-        ++$j;
+        for ($i = 0; $i < 100; $i++) {
+            $j++;
+        }
+
+        $iterations++;
     }
 
-    $i = 0;
-
-    while ($i < $iterations)
-        ++$i;
-
-    return microtime(true) - $time_start;
+    return $iterations;
 }
 
 
 /**
  * Test if else
- * @param  int $iterations
- * @return int elapsed time
+ * @param  int $limit time limit in microseconds
+ * @return int iterations done in allocated time
  */
-function test_if_else($iterations = 11500000)
+function test_if_else($limit)
 {
     $time_start = microtime(true);
+    $time_limit = $time_start + $limit;
+    $iterations = 0;
 
+    $i = 0;
     $j = 0;
 
-    for ($i = 0; $i < $iterations; $i++) {
-        if ($i == -1) {
+    while (microtime(true) < $time_limit) {
+        if ($i % 2 == 0) {
             $j = 1;
         }
-        elseif ($i == -2) {
+        elseif ($i % 3 == 0) {
             $j = 2;
         }
-        else if ($i == -3) {
+        else
+        if ($i % 5 == 0) {
             $j = 3;
         }
         else {
             $j = 4;
         }
+
+        $iterations++;
     }
 
-    return microtime(true) - $time_start;
+    return $iterations;
 }
 
 
 /**
  * Test arrays
- * @param  int $iterations
- * @return int elapsed time
+ * @param  int $limit time limit in microseconds
+ * @return int iterations done in allocated time
  */
-function test_arrays($iterations = 48000)
+function test_arrays($limit)
 {
     $time_start = microtime(true);
+    $time_limit = $time_start + $limit;
+    $iterations = 0;
 
     $a = [];
 
-    for ($i = 0; $i < $iterations; $i++) {
+    while (microtime(true) < $time_limit) {
         array_push($a, [
             rand() => random_bytes(10)
         ]);
-    }
 
-    for ($i = 0; $i < $iterations; $i++) {
         array_search(random_bytes(10), $a, true);
+
+        $iterations++;
     }
 
-    return microtime(true) - $time_start;
+    return $iterations;
 }
 
 
 /**
  * Test cryptographic hashes
- * @param  int $iterations
- * @return int elapsed time
+ * @param  int $limit time limit in microseconds
+ * @return int iterations done in allocated time
  */
-function test_hashes($iterations = 105000)
+function test_hashes($limit)
 {
     $time_start = microtime(true);
+    $time_limit = $time_start + $limit;
+    $iterations = 0;
 
     $hashes = ['adler32', 'crc32', 'crc32b', 'md5', 'sha1', 'sha256', 'sha384', 'sha512'];
     $string = random_bytes(1024);
 
-    foreach ($hashes as $hash) {
-        for ($i = 0; $i < $iterations; $i++) {
+    while (microtime(true) < $time_limit) {
+        foreach ($hashes as $hash) {
             hash($hash, $string, false);
         }
+
+        $iterations++;
     }
 
-    return microtime(true) - $time_start;
+    return $iterations;
 }
 
 
 /**
  * Test file operations
- * @param  int $iterations
- * @return int elapsed time or -1 on failure
+ * @param  int $limit time limit in microseconds
+ * @return int iterations done in allocated time or false on failure
  */
-function test_files($iterations = 500)
+function test_files($limit)
 {
     $time_start = microtime(true);
+    $time_limit = $time_start + $limit;
+    $iterations = 0;
 
     // max number of bytes to write
     $bytes_to_write_max = 0.5 * 1024 * 1024;
@@ -241,24 +279,24 @@ function test_files($iterations = 500)
     // get temporary directory
     $tmp_dir = sys_get_temp_dir() . DIRECTORY_SEPARATOR;
 
-    for ($i = 0; $i < $iterations; $i++) {
+    while (microtime(true) < $time_limit) {
         // scan temp dir
         $list = scandir($tmp_dir);
 
         if (!$list)
-            return -1;
+            return false;
 
         // get temporary file name in temporary dir
         $tmp_filename = tempnam($tmp_dir, '');
 
         if (!$tmp_filename)
-            return -1;
+            return false;
 
         // open temp file
         $handle = fopen($tmp_filename, 'r+');
 
         if (!$handle)
-            return -1;
+            return false;
 
         // get bytes count to write to file
         $bytes_to_write = rand(1, $bytes_to_write_max);
@@ -290,21 +328,25 @@ function test_files($iterations = 500)
 
         // delete file
         unlink($tmp_filename);
+
+        $iterations++;
     }
 
     //echo('total bytes : '. format_bytes($total_bytes) ."\n");
-    return microtime(true) - $time_start;
+    return $iterations;
 }
 
 
 /**
  * Test mysql operations
- * @param  int    $iterations
- * @return int elapsed time or -1 on error
+ * @param  int $limit time limit in microseconds
+ * @return int iterations done in allocated time or false on failure
  */
-function test_mysql($iterations = 700)
+function test_mysql($limit)
 {
     $time_start = microtime(true);
+    $time_limit = $time_start + $limit;
+    $iterations = 0;
 
     $host       = 'localhost';
     $user       = 'root';
@@ -315,59 +357,63 @@ function test_mysql($iterations = 700)
     $exception  = false;
 
     try {
-        // connect to database
-        $mysqli = mysqli_connect($host, $user, $pass);
+        while (microtime(true) < $time_limit) {
+            // connect to database
+            $mysqli = mysqli_connect($host, $user, $pass);
 
-        if (!$mysqli)
-            throw new Exception('Connect to database - FAILED');
+            if (!$mysqli)
+                throw new Exception('Connect to database - FAILED');
 
-        // check if database already exists
-        $query = <<<TAG
-            SELECT
-                SCHEMA_NAME
-            FROM
-                INFORMATION_SCHEMA.SCHEMATA
-            WHERE
-                SCHEMA_NAME = '{$db}'
-        TAG;
+            if (!$iterations) {
+                // check if database already exists
+                $query = <<<TAG
+                    SELECT
+                        SCHEMA_NAME
+                    FROM
+                        INFORMATION_SCHEMA.SCHEMATA
+                    WHERE
+                        SCHEMA_NAME = '{$db}'
+                TAG;
 
-        $result = mysqli_query($mysqli, $query);
+                $result = mysqli_query($mysqli, $query);
 
-        if (!$result)
-            throw new Exception('Check if database exists - FAILED');
+                if (!$result)
+                    throw new Exception('Check if database exists - FAILED');
 
-        $array = mysqli_fetch_array($result);
+                $array = mysqli_fetch_array($result);
 
-        if (isset($array))
-            throw new Exception('Database already exists');
+                if (isset($array))
+                    throw new Exception('Database already exists');
 
-        // create database
-        $query = <<<TAG
-            CREATE DATABASE `{$db}`;
-        TAG;
+                // create database
+                $query = <<<TAG
+                    CREATE DATABASE `{$db}`;
+                TAG;
 
-        if (!mysqli_query($mysqli, $query))
-            throw new Exception('Create database - FAILED');
+                if (!mysqli_query($mysqli, $query))
+                    throw new Exception('Create database - FAILED');
 
-        $db_created = true;
+                $db_created = true;
+            }
 
-        // select database
-        if (!mysqli_select_db($mysqli, $db))
-            throw new Exception('Select database - FAILED');
+            // select database
+            if (!mysqli_select_db($mysqli, $db))
+                throw new Exception('Select database - FAILED');
 
-        // create table
-        $table = 'test';
-        $query = <<<TAG
-            CREATE TABLE `{$table}` (
-                `date` timestamp NOT NULL,
-                `string` varchar(512) NOT NULL
-            );
-        TAG;
+            if (!$iterations) {
+                // create table
+                $table = 'test';
+                $query = <<<TAG
+                    CREATE TABLE `{$table}` (
+                        `date` timestamp NOT NULL,
+                        `string` varchar(512) NOT NULL
+                    );
+                TAG;
 
-        if (!mysqli_query($mysqli, $query))
-            throw new Exception('Create table - FAILED');
+                if (!mysqli_query($mysqli, $query))
+                    throw new Exception('Create table - FAILED');
+            }
 
-        for ($i = 0; $i < $iterations; $i++) {
             // insert into table
             $str = bin2hex(random_bytes(rand(1, 256)));
 
@@ -400,6 +446,11 @@ function test_mysql($iterations = 700)
 
             if (!$array)
                 throw new Exception('Select from table - FAILED');
+
+            // disconnect from database
+            mysqli_close($mysqli);
+
+            $iterations++;
         }
     }
     catch (Exception $e) {
@@ -409,7 +460,11 @@ function test_mysql($iterations = 700)
     finally {
         // check for connection failure
         if (!$mysqli)
-            return -1;
+            return false;
+
+        if (!$exception)
+            // connect to database
+            $mysqli = mysqli_connect($host, $user, $pass);
 
         if ($db_created) {
             // drop database
@@ -424,22 +479,19 @@ function test_mysql($iterations = 700)
         mysqli_close($mysqli);
     }
 
-    return $exception ? -1 : microtime(true) - $time_start;
+    return !$exception ? $iterations : false;
 }
 
 
 /**
- * Format time
- * @param  int $time
+ * Format number
+ * @param  int $number
  * @param  int $padding
- * @param  bool $add_unit
  * @return string
  */
-function format_time($time, $padding, $add_unit = false)
+function format_number($number, $padding)
 {
-    $str = number_format($time, 1) . ($add_unit ? ' s' : '');
-
-    return str_pad($str, $padding, ' ', STR_PAD_LEFT);
+    return str_pad(number_format($number, 0, '.', ''), $padding, ' ', STR_PAD_LEFT);
 }
 
 
@@ -516,7 +568,7 @@ function median(array $cells)
 function interval($cells)
 {
     // interval
-    return '['. format_time(min($cells), 0) .' - '. format_time(max($cells), 0) .']';
+    return '['. format_number(min($cells), 0) .' - '. format_number(max($cells), 0) .']';
 }
 
 
@@ -546,7 +598,7 @@ function show_all($cells)
     $str = '';
 
     foreach ($cells as $cell) {
-        $str .= format_time($cell, 0) .' ';
+        $str .= format_number($cell, 0) .' ';
     }
 
     return $str;
