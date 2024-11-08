d9325fcd4f4c4a47711e67a31f89d93b7de4d575
8ctopus <hello@octopuslabs.io>
2024-11-08 09:17:57 +0400

    Refactor benchmark into class

--- src/Benchmark.php
+++ src/Benchmark.php
@@ -12,213 +12,245 @@ if (PHP_SAPI !== 'cli') {
     throw new Exception('Please run the script from cli');
 }
 
-$settings = [
-    'iterations' => 500,
-    'time_per_iteration' => 20,
+(new Benchmark($argv))
+    ->run();
 
-    'test_filter' => '/^test/',
-    'custom_tests' => false,
+class Benchmark
+{
+    private array $argv;
+    private array $settings;
+    private readonly string $line;
 
-    'compare' => false,
+    public function __construct(array $argv)
+    {
+        $this->argv = $argv;
 
-    'show_histogram' => false,
-    'histogram_buckets' => 16,
-    'histogram_bar_width' => 50,
+        $this->settings = [
+            'iterations' => 500,
+            'time_per_iteration' => 20,
 
-    'show_outliers' => false,
-    'show_all_measurements' => false,
+            'test_filter' => '/^test/',
+            'custom_tests' => false,
 
-    'save' => false,
-    'save_filename' => '',
-    'save_filename_base' => 'benchmark_',
-    'save_filename_ext' => date('Ymd-Hi') . '.txt',
-];
+            'compare' => false,
 
-// get command line arguments
-for ($i = 1; $i < count($argv); ++$i) {
-    $argument = $argv[$i];
+            'show_histogram' => false,
+            'histogram_buckets' => 16,
+            'histogram_bar_width' => 50,
 
-    if (strpos($argument, '--') !== 0) {
-        throw new Exception("unknown argument - {$argument}");
+            'show_outliers' => false,
+            'show_all_measurements' => false,
+
+            'save' => false,
+            'save_filename' => '',
+            'save_filename_base' => 'benchmark_',
+            'save_filename_ext' => date('Ymd-Hi') . '.txt',
+        ];
+
+        $this->line = str_pad('', Helper::$pad1 + Helper::$pad2 + 3, '-');
     }
 
-    switch ($argument) {
-        case '--compare':
-            $i++;
-            $settings['compare'] = $argv[$i];
-            break;
+    public function run() : void
+    {
+        $this->readCommandLine($this->argv);
 
-        case '--custom':
-            $settings['custom_tests'] = true;
-            break;
+        $settings = $this->settings;
 
-        case '--filter':
-            $i++;
-            $settings['test_filter'] = $argv[$i];
-            break;
+        $this->showTitle();
 
-        case '--histogram':
-            $settings['show_histogram'] = true;
-            break;
+        $class = $settings['custom_tests'] ? TestsUser::class : Tests::class;
 
-        case '--histogram-buckets':
-            $i++;
-            $settings['histogram_buckets'] = $argv[$i];
-            break;
+        $tests = $this->getTests($class, $settings['test_filter']);
 
-        case '--histogram-width':
-            $i++;
-            $settings['histogram_bar_width'] = $argv[$i];
-            break;
+        $reports = $this->runTests($class, $tests, $settings['iterations'], (float) $settings['time_per_iteration']);
 
-        case '--iterations':
-            $i++;
-            $settings['iterations'] = $argv[$i];
-            break;
+        if ($settings['save']) {
+            $this->saveReports($reports);
+        }
 
-        case '--save':
-            $settings['save'] = true;
-            if (!empty($argv[$i + 1]) && strpos($argv[$i + 1], '--') === false) {
-                ++$i;
-                $settings['save_filename'] = $settings['save_filename_base'] . $argv[$i] . '_' . $settings['save_filename_ext'];
+        if ($settings['custom_tests'] && count($tests) % 2 === 0) {
+            $baseline = (new Reports())
+                ->addReport($reports[0]);
+
+            $update = (new Reports())
+                ->addReport($reports[1]);
+
+            Helper::showCompare($baseline, $update);
+        } elseif ($settings['compare']) {
+            $baseline = unserialize(file_get_contents($settings['compare']));
+            Helper::showCompare($baseline, $reports);
+        } else {
+            Helper::showBenchmark($reports, $settings);
+        }
+    }
+
+    private function runTests(string $class, array $testsAsc, int $iterations, float $timePerIteration) : Reports
+    {
+        $testsDesc = $testsAsc;
+        krsort($testsDesc);
+
+        $reports = new Reports();
+
+        for ($i = 0; $i < $iterations; ++$i) {
+            $this->updateProgress($i / $iterations);
+
+            // switch testing order
+            $tests = $i % 2 ? $testsDesc : $testsAsc;
+
+            foreach ($tests as $test) {
+                $measurement = $this->runTest($class, $test, $timePerIteration);
+
+                $reports->add($test, $measurement);
+            }
+        }
+
+        return $reports;
+    }
+
+    private function runTest(string $class, string $test, float $timePerIteration) : int
+    {
+        // burn the first test
+        $class::$test();
+
+        $iterations = 0;
+        $timeLimit = hrtime(true) + $timePerIteration * 1000000;
+
+        while (hrtime(true) < $timeLimit) {
+            $class::$test();
+            ++$iterations;
+        }
+
+        return $iterations;
+    }
+
+    private function getTests(string $class, string $filter) : array
+    {
+        $tests = get_class_methods($class);
+
+        // filter tests
+        foreach ($tests as $index => $test) {
+            if (preg_match($filter, $test) !== 1) {
+                // remove not matching test
+                unset($tests[$index]);
+            }
+        }
+
+        // reset array keys
+        return array_values($tests);
+    }
+
+    private function updateProgress(float $percentage) : void
+    {
+        $progress = Helper::formatPercentage($percentage, false, 3);
+        $text = "Running tests {$progress}...";
+        $length = strlen($text);
+
+        echo "{$text}\033[{$length}D";
+    }
+
+    private function saveReports(Reports $reports) : void
+    {
+        if (empty($this->settings['save_filename'])) {
+            $this->settings['save_filename'] = $this->settings['save_filename_base'] . $this->settings['save_filename_ext'];
+        }
+
+        file_put_contents($this->settings['save_filename'], serialize($reports));
+
+        echo "benchmark saved to {$this->settings['save_filename']}\n";
+        echo "{$this->line}\n";
+    }
+
+    private function readCommandLine(array $argv) : void
+    {
+        $settings = $this->settings;
+
+        // get command line arguments
+        for ($i = 1; $i < count($argv); ++$i) {
+            $argument = $argv[$i];
+
+            if (strpos($argument, '--') !== 0) {
+                throw new Exception("unknown argument - {$argument}");
             }
 
-            break;
+            switch ($argument) {
+                case '--compare':
+                    $i++;
+                    $settings['compare'] = $argv[$i];
+                    break;
 
-        case '--show-all':
-            $settings['show_all_measurements'] = true;
-            break;
+                case '--custom':
+                    $settings['custom_tests'] = true;
+                    break;
 
-        case '--show-outliers':
-            $settings['show_outliers'] = true;
-            break;
+                case '--filter':
+                    $i++;
+                    $settings['test_filter'] = $argv[$i];
+                    break;
 
-        case '--time-per-iteration':
-            $i++;
-            $settings['time_per_iteration'] = $argv[$i];
-            break;
+                case '--histogram':
+                    $settings['show_histogram'] = true;
+                    break;
 
-        default:
-            throw new Exception("unknown argument - {$argument}");
-    }
-}
+                case '--histogram-buckets':
+                    $i++;
+                    $settings['histogram_buckets'] = $argv[$i];
+                    break;
 
-$line = str_pad('', Helper::$pad1 + Helper::$pad2 + 3, '-');
+                case '--histogram-width':
+                    $i++;
+                    $settings['histogram_bar_width'] = $argv[$i];
+                    break;
 
-$totalTime = $settings['iterations'] * $settings['time_per_iteration'] / 1000;
+                case '--iterations':
+                    $i++;
+                    $settings['iterations'] = $argv[$i];
+                    break;
 
-echo "PHP benchmark\n\n" .
-    "{$line}\n" .
-    str_pad('platform', Helper::$pad1) . ' : ' . str_pad(PHP_OS . ' ' . ((PHP_INT_SIZE === 8) ? 'x64' : 'x32'), Helper::$pad2, ' ', STR_PAD_LEFT) . "\n" .
-    str_pad('php version', Helper::$pad1) . ' : ' . str_pad(PHP_VERSION, Helper::$pad2, ' ', STR_PAD_LEFT) . "\n" .
-    str_pad('xdebug', Helper::$pad1) . ' : ' . str_pad(extension_loaded('xdebug') ? 'on' : 'off', Helper::$pad2, ' ', STR_PAD_LEFT) . "\n" .
-    str_pad('opcache', Helper::$pad1) . ' : ' . str_pad((extension_loaded('Zend OPcache') && ini_get('opcache.enable_cli')) ? 'on' : 'off', Helper::$pad2, ' ', STR_PAD_LEFT) . "\n" .
-    str_pad('memory limit', Helper::$pad1) . ' : ' . str_pad(ini_get('memory_limit'), Helper::$pad2, ' ', STR_PAD_LEFT) . "\n" .
-    str_pad('max execution', Helper::$pad1) . ' : ' . str_pad(ini_get('max_execution_time'), Helper::$pad2, ' ', STR_PAD_LEFT) . "\n" .
-    str_pad('iterations', Helper::$pad1) . ' : ' . str_pad((string) $settings['iterations'], Helper::$pad2, ' ', STR_PAD_LEFT) . "\n" .
-    str_pad('time per iteration', Helper::$pad1) . ' : ' . str_pad($settings['time_per_iteration'] . 'ms', Helper::$pad2, ' ', STR_PAD_LEFT) . "\n" .
-    str_pad('total time per test', Helper::$pad1) . ' : ' . str_pad($totalTime . 's', Helper::$pad2, ' ', STR_PAD_LEFT) . "\n" .
-    "{$line}\n";
+                case '--save':
+                    $settings['save'] = true;
+                    if (!empty($argv[$i + 1]) && strpos($argv[$i + 1], '--') === false) {
+                        ++$i;
+                        $settings['save_filename'] = $settings['save_filename_base'] . $argv[$i] . '_' . $settings['save_filename_ext'];
+                    }
 
-$class = $settings['custom_tests'] ? TestsUser::class : Tests::class;
+                    break;
 
-$tests = getTests($class, $settings['test_filter']);
+                case '--show-all':
+                    $settings['show_all_measurements'] = true;
+                    break;
 
-$reports = runTests($class, $tests, $settings['iterations'], (float) $settings['time_per_iteration']);
+                case '--show-outliers':
+                    $settings['show_outliers'] = true;
+                    break;
 
-if ($settings['save']) {
-    saveReports($reports);
-}
+                case '--time-per-iteration':
+                    $i++;
+                    $settings['time_per_iteration'] = $argv[$i];
+                    break;
 
-if ($settings['custom_tests'] && count($tests) % 2 === 0) {
-    $baseline = (new Reports())
-        ->addReport($reports[0]);
-
-    $update = (new Reports())
-        ->addReport($reports[1]);
-
-    Helper::showCompare($baseline, $update);
-} elseif ($settings['compare']) {
-    $baseline = unserialize(file_get_contents($settings['compare']));
-    Helper::showCompare($baseline, $reports);
-} else {
-    Helper::showBenchmark($reports, $settings);
-}
-
-function runTests(string $class, array $testsAsc, int $iterations, float $timePerIteration) : Reports
-{
-    $testsDesc = $testsAsc;
-    krsort($testsDesc);
-
-    $reports = new Reports();
-
-    for ($i = 0; $i < $iterations; ++$i) {
-        updateProgress($i / $iterations);
-
-        // switch testing order
-        $tests = $i % 2 ? $testsDesc : $testsAsc;
-
-        foreach ($tests as $test) {
-            $measurement = runTest($class, $test, $timePerIteration);
-
-            $reports->add($test, $measurement);
+                default:
+                    throw new Exception("unknown argument - {$argument}");
+            }
         }
     }
 
-    return $reports;
-}
+    private function showTitle() : void
+    {
+        $settings = $this->settings;
 
-function runTest(string $class, string $test, float $timePerIteration) : int
-{
-    // burn the first test
-    $class::$test();
+        $totalTime = $settings['iterations'] * $settings['time_per_iteration'] / 1000;
 
-    $iterations = 0;
-    $timeLimit = hrtime(true) + $timePerIteration * 1000000;
-
-    while (hrtime(true) < $timeLimit) {
-        $class::$test();
-        ++$iterations;
+        echo "PHP benchmark\n\n" .
+            "{$this->line}\n" .
+            str_pad('platform', Helper::$pad1) . ' : ' . str_pad(PHP_OS . ' ' . ((PHP_INT_SIZE === 8) ? 'x64' : 'x32'), Helper::$pad2, ' ', STR_PAD_LEFT) . "\n" .
+            str_pad('php version', Helper::$pad1) . ' : ' . str_pad(PHP_VERSION, Helper::$pad2, ' ', STR_PAD_LEFT) . "\n" .
+            str_pad('xdebug', Helper::$pad1) . ' : ' . str_pad(extension_loaded('xdebug') ? 'on' : 'off', Helper::$pad2, ' ', STR_PAD_LEFT) . "\n" .
+            str_pad('opcache', Helper::$pad1) . ' : ' . str_pad((extension_loaded('Zend OPcache') && ini_get('opcache.enable_cli')) ? 'on' : 'off', Helper::$pad2, ' ', STR_PAD_LEFT) . "\n" .
+            str_pad('memory limit', Helper::$pad1) . ' : ' . str_pad(ini_get('memory_limit'), Helper::$pad2, ' ', STR_PAD_LEFT) . "\n" .
+            str_pad('max execution', Helper::$pad1) . ' : ' . str_pad(ini_get('max_execution_time'), Helper::$pad2, ' ', STR_PAD_LEFT) . "\n" .
+            str_pad('iterations', Helper::$pad1) . ' : ' . str_pad((string) $settings['iterations'], Helper::$pad2, ' ', STR_PAD_LEFT) . "\n" .
+            str_pad('time per iteration', Helper::$pad1) . ' : ' . str_pad($settings['time_per_iteration'] . 'ms', Helper::$pad2, ' ', STR_PAD_LEFT) . "\n" .
+            str_pad('total time per test', Helper::$pad1) . ' : ' . str_pad($totalTime . 's', Helper::$pad2, ' ', STR_PAD_LEFT) . "\n" .
+            "{$this->line}\n";
     }
-
-    return $iterations;
-}
-
-function getTests(string $class, string $filter) : array
-{
-    $tests = get_class_methods($class);
-
-    // filter tests
-    foreach ($tests as $index => $test) {
-        if (preg_match($filter, $test) !== 1) {
-            // remove not matching test
-            unset($tests[$index]);
-        }
-    }
-
-    // reset array keys
-    return array_values($tests);
-}
-
-function updateProgress(float $percentage) : void
-{
-    $progress = Helper::formatPercentage($percentage, false, 3);
-    $text = "Running tests {$progress}...";
-    $length = strlen($text);
-
-    echo "{$text}\033[{$length}D";
-}
-
-function saveReports(Reports $reports) : void
-{
-    global $settings, $line;
-
-    if (empty($settings['save_filename'])) {
-        $settings['save_filename'] = $settings['save_filename_base'] . $settings['save_filename_ext'];
-    }
-
-    file_put_contents($settings['save_filename'], serialize($reports));
-
-    echo "benchmark saved to {$settings['save_filename']}\n";
-    echo "{$line}\n";
 }
