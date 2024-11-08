0c70ebcf65962e4100f7c15700aad7e4899abea5
8ctopus <hello@octopuslabs.io>
2024-11-08 15:44:55 +0400

    Fix mysql tests

--- src/TestsMySql.php
+++ src/TestsMySql.php
@@ -8,163 +8,96 @@ use Exception;
 
 class TestsMySql
 {
-    /**
-     * Test mysql operations
-     *
-     * @param float $limit time limit in seconds
-     *
-     * @return int iterations done in allocated time or null on failure
-     */
-    public static function testMySql(float $limit) : ?int
+    public static function testMySql() : void
     {
-        $timeStarted = microtime(true);
-        $timeLimit = $timeStarted + $limit;
-        $iterations = 0;
-
         $host = 'localhost';
         $user = 'root';
         $pass = '123';
         $db = 'benchmark-test';
-        $mysqli = null;
-        $dbCreated = false;
-        $exception = false;
 
         if (!function_exists('mysqli_connect')) {
-            return null;
+            throw new Exception('mysqli');
         }
 
-        try {
-            while (microtime(true) < $timeLimit) {
-                // connect to database
-                $mysqli = mysqli_connect($host, $user, $pass);
+        // connect to database
+        $mysqli = mysqli_connect($host, $user, $pass);
 
-                if (!$mysqli) {
-                    throw new Exception('Connect to database');
-                }
-
-                if (!$iterations) {
-                    // check if database already exists
-                    $query = <<<TAG
-                    SELECT
-                        SCHEMA_NAME
-                    FROM
-                        INFORMATION_SCHEMA.SCHEMATA
-                    WHERE
-                        SCHEMA_NAME = '{$db}'
-                    TAG;
-
-                    $result = mysqli_query($mysqli, $query);
-
-                    if (!$result) {
-                        throw new Exception('Check if database exists');
-                    }
-
-                    $array = mysqli_fetch_array($result);
-
-                    if (isset($array)) {
-                        throw new Exception('Database already exists');
-                    }
-
-                    // create database
-                    $query = <<<TAG
-                    CREATE DATABASE `{$db}`;
-                    TAG;
-
-                    if (!mysqli_query($mysqli, $query)) {
-                        throw new Exception('Create database');
-                    }
-
-                    $dbCreated = true;
-                }
-
-                // select database
-                if (!mysqli_select_db($mysqli, $db)) {
-                    throw new Exception('Select database');
-                }
-
-                if (!$iterations) {
-                    // create table
-                    $table = 'test';
-                    $query = <<<TAG
-                    CREATE TABLE `{$table}` (
-                        `date` timestamp NOT NULL,
-                        `string` varchar(512) NOT NULL
-                    );
-                    TAG;
-
-                    if (!mysqli_query($mysqli, $query)) {
-                        throw new Exception('Create table');
-                    }
-                }
-
-                // insert into table
-                $str = bin2hex(Helper::notRandomBytes(rand(1, 256)));
-
-                $query = <<<TAG
-                INSERT INTO
-                    `{$table}` (`date`, `string`)
-                VALUES
-                    (CURRENT_TIMESTAMP, '{$str}');
-                TAG;
-
-                if (!mysqli_query($mysqli, $query)) {
-                    throw new Exception('Insert into table');
-                }
-
-                // select from table
-                $query = <<<TAG
-                SELECT
-                    *
-                FROM
-                    `{$table}`
-                WHERE
-                    1;
-                TAG;
-
-                $result = mysqli_query($mysqli, $query);
-
-                if (!$result) {
-                    throw new Exception('Select from table');
-                }
-
-                $array = mysqli_fetch_array($result);
-
-                if (!$array) {
-                    throw new Exception('Select from table');
-                }
-
-                // disconnect from database
-                mysqli_close($mysqli);
-
-                ++$iterations;
-            }
-        } catch (Exception $exception) {
-            $exception = true;
-            //echo($exception->getMessage() ."\n");
-        } finally {
-            // check for connection failure
-            if (!$mysqli) {
-                return null;
-            }
-
-            if (!$exception) {
-                // connect to database
-                $mysqli = mysqli_connect($host, $user, $pass);
-            }
-
-            if ($dbCreated) {
-                // drop database
-                $query = <<<TAG
-                DROP DATABASE `{$db}`;
-                TAG;
-
-                mysqli_query($mysqli, $query);
-            }
-
-            // disconnect from database
-            mysqli_close($mysqli);
+        if (!$mysqli) {
+            throw new Exception('Connect to database');
         }
 
-        return $exception ? null : $iterations;
+        // create database
+        $query = <<<TAG
+        CREATE DATABASE IF NOT EXISTS `{$db}`;
+        TAG;
+
+        if (!mysqli_query($mysqli, $query)) {
+            throw new Exception('Create database');
+        }
+
+        // select database
+        if (!mysqli_select_db($mysqli, $db)) {
+            throw new Exception('Select database');
+        }
+
+        $table = 'test';
+
+        // create table
+        $query = <<<TAG
+        CREATE TABLE IF NOT EXISTS `{$table}` (
+            `date` timestamp NOT NULL,
+            `string` varchar(512) NOT NULL
+        );
+        TAG;
+
+        if (!mysqli_query($mysqli, $query)) {
+            throw new Exception('Create table');
+        }
+
+        // insert into table
+        $str = bin2hex(Helper::notRandomBytes(rand(1, 256)));
+
+        $query = <<<TAG
+        INSERT INTO
+            `{$table}` (`date`, `string`)
+        VALUES
+            (CURRENT_TIMESTAMP, '{$str}');
+        TAG;
+
+        if (!mysqli_query($mysqli, $query)) {
+            throw new Exception('Insert into table');
+        }
+
+        // select from table
+        $query = <<<TAG
+        SELECT
+            *
+        FROM
+            `{$table}`
+        WHERE
+            1;
+        TAG;
+
+        $result = mysqli_query($mysqli, $query);
+
+        if (!$result) {
+            throw new Exception('Select from table');
+        }
+
+        $array = mysqli_fetch_array($result);
+
+        if (!$array) {
+            throw new Exception('Select from table');
+        }
+
+        // drop database
+        $query = <<<TAG
+        DROP DATABASE `{$db}`;
+        TAG;
+
+        mysqli_query($mysqli, $query);
+
+        // disconnect from database
+        mysqli_close($mysqli);
     }
 }
