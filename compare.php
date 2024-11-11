11de169dede7df6f3019ba5df18b49072fbe9f1a
8ctopus <hello@octopuslabs.io>
2024-11-11 09:08:46 +0400

    Cleanup compare code

--- compare.php
+++ compare.php
@@ -9,15 +9,11 @@ use Exception;
 require_once __DIR__ . '/vendor/autoload.php';
 
 if (PHP_SAPI !== 'cli') {
-    throw new Exception('run from cli');
+    throw new Exception('Please run the script from cli');
 }
 
 for ($i = 1; $i < count($argv); ++$i) {
     $argument = $argv[$i];
-
-    if (strpos($argument, '--') !== 0) {
-        throw new Exception("unknown argument - {$argument}");
-    }
 
     switch ($argument) {
         case '--file1':
@@ -35,11 +31,8 @@ for ($i = 1; $i < count($argv); ++$i) {
     }
 }
 
-if (!isset($file1) || !isset($file2) || !file_exists($file1) || !file_exists($file2)) {
+if (!isset($file1, $file2) || !file_exists($file1) || !file_exists($file2)) {
     throw new Exception('both files must exist');
 }
 
-$data1 = unserialize(file_get_contents($file1));
-$data2 = unserialize(file_get_contents($file2));
-
-Helper::showCompare($data1, $data2);
+Helper::showCompare(unserialize(file_get_contents($file1)), unserialize(file_get_contents($file2)));
